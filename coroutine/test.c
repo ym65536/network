af0b23f179c4c37acfeb4a62d532ec7d396e2a0a
ym65536 <ym65536@126.com>
2016-08-03 20:39:04 +0800

    modify test

--- coroutine/test.c
+++ coroutine/test.c
@@ -1,30 +1,24 @@
 #include "common.h"
 
-st_thread_t* stid1 = NULL;
-st_thread_t* stid2 = NULL;
+#define MAX_THREAD (10 * 1024)
+//#define MAX_THREAD (4)
 
-void thread1(void* args)
+struct thread_data
 {
-	char* data = (char* )args;
+	int seqno;
+	char data[0x100];
+	st_thread_t* stid;
+};
+struct thread_data tdata[MAX_THREAD];
+
+void thread(void* args)
+{
+	struct thread_data* pt = (struct thread_data* )args;
 	st_thread_t* ctid = ST_CURRENT_THREAD();
-	static int seq = 0;
-	printf("T1 ENTER: data=%s, seqno=%d, ctid=%08x, stid1=%08x, stid2=%08x\n", data, seq, (unsigned int)ctid, (unsigned int)stid1, (unsigned int)stid2);
-	seq++;
+	printf("ENTER: data=%s, seqno=%d, ctid=%08x, stid1=%08x\n", pt->data, pt->seqno, (unsigned int)ctid, (unsigned int)pt->stid);
 
 	ST_SWITCH_CONTEXT(ctid);
-	printf("T1 OUT: data=%s, seqno=%d, ctid=%08x, stid1=%08x, stid2=%08x\n", data, seq, (unsigned int)ctid, (unsigned int)stid1, (unsigned int)stid2);
-}
-
-void thread2(void* args)
-{
-	char* data = (char* )args;
-	st_thread_t* ctid = ST_CURRENT_THREAD();
-	static int seq = 0;
-	printf("T2 ENTER: data=%s, seqno=%d, ctid=%08x, stid1=%08x, stid2=%08x\n", data, seq, (unsigned int)ctid, (unsigned int)stid1, (unsigned int)stid2);
-	seq++;
-
-	ST_SWITCH_CONTEXT(ctid);
-	printf("T2 OUT: data=%s, seqno=%d, ctid=%08x, stid1=%08x, stid2=%08x\n", data, seq, (unsigned int)ctid, (unsigned int)stid1, (unsigned int)stid2);
+	printf("OUTER: data=%s, seqno=%d, ctid=%08x, stid1=%08x\n", pt->data, pt->seqno, (unsigned int)ctid, (unsigned int)pt->stid);
 }
 
 int main(void)
@@ -34,11 +28,16 @@ int main(void)
 		printf("state thread init fail!\n");
 		return -1;
 	}
+	 
+	int i = 0;
+	for (i = 0; i < MAX_THREAD; i++)
+	{
+		tdata[i].seqno = i;
+		sprintf(tdata[i].data, "I am in thread %d\n", i);
+		tdata[i].stid = (st_thread_t* )st_thread_create(thread, (void* )&tdata[i]);
+	}
 
-	stid1 = (st_thread_t* )st_thread_create(thread1, (void* )"this is thread1!");
-	stid2 = (st_thread_t* )st_thread_create(thread2, (void* )"this is thread2!");
-
-	ST_RESTORE_CONTEXT(stid1);
+	ST_RESTORE_CONTEXT(tdata[0].stid);
 
 	return 0;
 }
